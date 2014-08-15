beef700e8199a7cf1eb8642e79af7e4f6774826e
hua <467953673@qq.com>
2014-08-15 12:13:39 +0800

    8.15

--- src/com/weixuan/football/fragment/MScoreboardFragment.java
+++ src/com/weixuan/football/fragment/MScoreboardFragment.java
@@ -1,14 +1,23 @@
 package com.weixuan.football.fragment;
 
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ListView;
+import com.alibaba.fastjson.JSON;
+import com.alibaba.fastjson.JSONArray;
+import com.litesuits.http.exception.HttpException;
+import com.litesuits.http.request.Request;
+import com.litesuits.http.request.param.HttpMethod;
+import com.litesuits.http.response.Response;
+import com.litesuits.http.response.handler.HttpModelHandler;
 import com.weixuan.football.R;
 import com.weixuan.football.adapter.ListScoreboardAdapater;
 import com.weixuan.football.entity.Standings;
+import com.weixuan.football.util.Dict;
+import com.weixuan.football.util.UrlApi;
+import com.weixuan.football.util.Util;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,33 +38,40 @@ public class MScoreboardFragment extends BaseFragment{
 			Bundle savedInstanceState) {
 		View view = inflater.inflate(R.layout.f_m_scoreboard, container,false);
 		initView(view, inflater);
+        requestList();
 		return view;
 	}
 
-    /****
-     * 初始化
-     * @param view
-     * @param inflater
-     */
+
+    private void requestList(){
+        Request req = new Request(Util.jointUrl(UrlApi.LIST,UrlApi.WEB_MODEL_CSL)).setMethod(HttpMethod.Get).
+                addUrlParam(Dict.ACCESS_TOKEN, application.getToken().getAccess_token());
+
+        asyncExecutor.execute(req,new HttpModelHandler<String>() {
+            @Override
+            protected void onSuccess(String str, Response res) {
+                System.out.println(str+"========");
+             List<Standings> ls=new ArrayList<Standings>();
+              JSONArray array=JSON.parseArray(str) ;   //parseArray(str,Standings.class);
+
+             ls.add(array.getObject(0,Standings.class));
+              //  System.out.println(ls);
+              adapter.notifyDataSetInvalidated();
+            }
+
+            @Override
+            protected void onFailure(HttpException e, Response res) {
+
+            }
+        });
+
+    }
+
+
     @Override
 	protected void initView(View view, LayoutInflater inflater) {
         listView=(ListView)view.findViewById(R.id.ls_data);
-
         data=new ArrayList<Standings>();
-        Standings standings=new Standings();
-        standings.setNo(1);
-        standings.setJsq("22/13");
-        standings.setDraw(2);
-        standings.setLose(3);
-        standings.setScore(55);
-        standings.setTeam("上海申鑫");
-        standings.setWin(20);
-        standings.setSession(12);
-        data.add(standings);
-        data.add(standings);
-        data.add(standings);
-
-
         adapter=new ListScoreboardAdapater(getActivity(),data);
         listView.setAdapter(adapter);
 
